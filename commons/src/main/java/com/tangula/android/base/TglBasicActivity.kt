ce18b10ddf89ca367dac9b9929a45822a78334fc
临碣 <wangds2016@163.com>
2018-10-15 21:37:00 +0800

    TglBasicActivity as abstract class

--- commons/src/main/java/com/tangula/android/base/TglBasicActivity.kt
+++ commons/src/main/java/com/tangula/android/base/TglBasicActivity.kt
@@ -1,16 +1,9 @@
 package com.tangula.android.base
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import com.tangula.android.utils.PermissionUtils
-import java.security.Permission
 
-class TglBasicActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-    }
+abstract class TglBasicActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
