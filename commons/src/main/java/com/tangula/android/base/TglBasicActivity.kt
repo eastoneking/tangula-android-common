acc5032e8441ea0bae007f3e90f5e2322d10a71a
临碣 <wangds2016@163.com>
2018-10-18 15:45:55 +0800

    Activity启动时自动设置ApplicationUtils中的APP属性

--- commons/src/main/java/com/tangula/android/base/TglBasicActivity.kt
+++ commons/src/main/java/com/tangula/android/base/TglBasicActivity.kt
@@ -1,9 +1,19 @@
 package com.tangula.android.base
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.tangula.android.utils.ApplicationUtils
 import com.tangula.android.utils.PermissionUtils
 
 abstract class TglBasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        if(ApplicationUtils.APP==null){
+            ApplicationUtils.APP = application
+        }
+        super.onCreate(savedInstanceState, persistentState)
+    }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
