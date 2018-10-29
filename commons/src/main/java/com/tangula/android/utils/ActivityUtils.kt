8c3c7670705a7ed6fbde9016e2b3796ce696db03
临碣 <wangds2016@163.com>
2018-10-29 10:34:26 +0800

    Activitiy工具

---
+++ commons/src/main/java/com/tangula/android/utils/ActivityUtils.kt
@@ -0,0 +1,20 @@
+package com.tangula.android.utils
+
+import android.app.Activity
+import android.content.Intent
+import kotlin.reflect.KClass
+
+
+@Suppress("unused")
+class ActivityUtils {
+
+    companion object {
+
+        @Suppress("unused")
+        fun startActivity(clazz:KClass<out Activity>){
+            ApplicationUtils.APP.startActivity(Intent(ApplicationUtils.APP, clazz.java))
+        }
+
+    }
+
+}
